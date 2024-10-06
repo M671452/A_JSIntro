7db0f778cbb17c22d5345b429a29609689d0730a
M671452 <112420978+M671452@users.noreply.github.com>
2024-10-06 15:41:38 +0200

    Add files via upload

---
+++ JSIntro/README.txt
@@ -0,0 +1,6 @@
+URL til applikasjonen er:
+http://127.0.0.1:8080/jsintro/
+
+Webdokumentene finner dere under mappen "src/main/resources/static".
+
+Se dokumentet om Eclipse for hvordan prosjektet kjøres.
---
+++ JSIntro/src/main/java/no/hvl/dat108/js/intro/JsIntroApplication.java
@@ -0,0 +1,13 @@
+package no.hvl.dat108.js.intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsIntroApplication.class, args);
+	}
+
+}
